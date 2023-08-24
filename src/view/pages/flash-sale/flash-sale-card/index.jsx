a7a3c6e79fe45b947870accb8c8ef678d38a4e39
phongdv-0421 <fongcypher@gmail.com>
2023-08-24 10:43:16 +0700

    update flash-sale-page

--- src/view/pages/flash-sale/flash-sale-card/index.jsx
+++ src/view/pages/flash-sale/flash-sale-card/index.jsx
@@ -15,7 +15,7 @@ function FlashSaleProduct (props) {
                 <div className="product-sale-display">
                     <div style={productStyle} className="product-display_image"></div>
                     <div className="product-sale-content">
-                        <div className="product-sale-content_name">[MÃ COSINNIJ - 8% ĐƠN 250K] SỮA RỬA MẶT SE KHÍT LỖ CHÂN LÔNG INNISFREE VOLCANIC PORE BHA CLEANSING FOAM 150G</div>
+                        <div className="product-sale-content_name">{props.name}</div>
                         <div className="product-sale-content_rating">
                             <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid UxAacW"><path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path></svg>
                             <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid UxAacW"><path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path></svg>
@@ -25,10 +25,10 @@ function FlashSaleProduct (props) {
                         </div>
                         <div className="product-sale-content_buy">
                             <div className='product-sale-content_buy_price'>
-                                <p>$500000</p>
+                                <p>{props.price}</p>
                             </div>
                             <div className='product-sale-content_buy_buybtn'>
-                                
+                                Mua ngay
                             </div>
                         </div>
                     </div>
--- src/view/pages/flash-sale/index.jsx
+++ src/view/pages/flash-sale/index.jsx
@@ -5,6 +5,7 @@ import Product from "components/product";
 import productApi from "api/productAPI";
 import { Link } from "react-router-dom";
 import FlashSaleProduct from "./flash-sale-card";
+
 function FlashSale(){
     
   const [products, setProducts] = useState([]);
@@ -12,7 +13,7 @@ function FlashSale(){
 
   useEffect(()=> {
       const fetchProducts = async () => {
-          const params = {_limit: 30}
+          const params = {_limit: 30, _page: 3}
           const productList = await productApi.getAllProduct(params);
           setProducts(productList.data);
       }
@@ -33,30 +34,23 @@ function FlashSale(){
             </div>
             <div className="flash-sale_products">
                 <ul>
-                    <li>
-                        <FlashSaleProduct
-                            imageURL = 'https://down-vn.img.susercontent.com/file/vn-50009109-60c7b81feb0aec3703fee2f702f96c54_tn'
-                        />
-                    </li>
+                    
                    
-                    {/* {products.map(post => {
+                    {products.map(post => {
                           return (
                           <Link key = {post.id} to={`/product-view/${post.id}`}>
                             <li>
-                              <Product 
-                                    imageURL={post.images[0]}
-                                    name={post.name}
-                                    discount ="giảm 25k"
-                                    price={"$"+post.salePrice}
-                                    height = "291px"
-                                    width = '457px'
-                                    sold="7,2k"  
-                                />
+                                {console.log(post)}
+                            <FlashSaleProduct
+                                imageURL = {post.images[0]}
+                                name = {post.name}
+                                price = {post.salePrice}
+                            />
                             </li>
                               
                           </Link>
                           )
-                      })} */}
+                      })}
                    
                 </ul>
             </div>
